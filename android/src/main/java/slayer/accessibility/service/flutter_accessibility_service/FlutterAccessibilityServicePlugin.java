1ada8f4233502092af3a111dfdcc8644ad441b21
Jaemin-VIRNECT <jmpark@virnect.com>
2025-03-12 19:02:50 +0900

    test 2

--- android/src/main/java/slayer/accessibility/service/flutter_accessibility_service/FlutterAccessibilityServicePlugin.java
+++ android/src/main/java/slayer/accessibility/service/flutter_accessibility_service/FlutterAccessibilityServicePlugin.java
@@ -162,7 +162,6 @@ public class FlutterAccessibilityServicePlugin implements FlutterPlugin, Activit
                 result.success(false);
             }
         }
-    }
      /*   else if (call.method.equals("performActionByText")) {
             String text = call.argument("text");
             Integer action = (Integer) call.argument("nodeAction");
@@ -188,112 +187,106 @@ public class FlutterAccessibilityServicePlugin implements FlutterPlugin, Activit
                 result.success(false);
             }
         } */
-        else if(call.method.equals("showOverlayWindow"))
-
-    {
-        if (!supportOverlay) {
-            result.error("ERR:OVERLAY", "Add the overlay entry point to be able of using it", null);
-            return;
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
-            AccessibilityListener.showOverlay();
+        else if (call.method.equals("showOverlayWindow")) {
+            if (!supportOverlay) {
+                result.error("ERR:OVERLAY", "Add the overlay entry point to be able of using it", null);
+                return;
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+                AccessibilityListener.showOverlay();
+                result.success(true);
+            } else {
+                result.success(false);
+            }
+        } else if (call.method.equals("hideOverlayWindow")) {
+            AccessibilityListener.removeOverlay();
             result.success(true);
         } else {
-            result.success(false);
+            result.notImplemented();
         }
-    } else if(call.method.equals("hideOverlayWindow"))
-
-    {
-        AccessibilityListener.removeOverlay();
-        result.success(true);
-    } else
-
-    {
-        result.notImplemented();
     }
-}
 
 
-@Override
-public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-    eventChannel.setStreamHandler(null);
-    context.unregisterReceiver(actionsReceiver);
-}
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+        eventChannel.setStreamHandler(null);
+        context.unregisterReceiver(actionsReceiver);
+    }
 
-@SuppressLint("WrongConstant")
-@Override
-public void onListen(Object arguments, EventChannel.EventSink events) {
-    if (Utils.isAccessibilitySettingsOn(context)) {
-        /// Set up receiver
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(ACCESSIBILITY_INTENT);
+    @SuppressLint("WrongConstant")
+    @Override
+    public void onListen(Object arguments, EventChannel.EventSink events) {
+        if (Utils.isAccessibilitySettingsOn(context)) {
+            /// Set up receiver
+            IntentFilter intentFilter = new IntentFilter();
+            intentFilter.addAction(ACCESSIBILITY_INTENT);
 
-        accessibilityReceiver = new AccessibilityReceiver(events);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            context.registerReceiver(accessibilityReceiver, intentFilter, Context.RECEIVER_EXPORTED);
-        } else {
-            context.registerReceiver(accessibilityReceiver, intentFilter);
+            accessibilityReceiver = new AccessibilityReceiver(events);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                context.registerReceiver(accessibilityReceiver, intentFilter, Context.RECEIVER_EXPORTED);
+            } else {
+                context.registerReceiver(accessibilityReceiver, intentFilter);
+            }
+
+            /// Set up listener intent
+            Intent listenerIntent = new Intent(context, AccessibilityListener.class);
+            context.startService(listenerIntent);
+            Log.i("AccessibilityPlugin", "Started the accessibility tracking service.");
         }
-
-        /// Set up listener intent
-        Intent listenerIntent = new Intent(context, AccessibilityListener.class);
-        context.startService(listenerIntent);
-        Log.i("AccessibilityPlugin", "Started the accessibility tracking service.");
     }
-}
 
-@Override
-public void onCancel(Object arguments) {
-    context.unregisterReceiver(accessibilityReceiver);
-    accessibilityReceiver = null;
-}
+    @Override
+    public void onCancel(Object arguments) {
+        context.unregisterReceiver(accessibilityReceiver);
+        accessibilityReceiver = null;
+    }
 
-@Override
-public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (requestCode == REQUEST_CODE_FOR_ACCESSIBILITY) {
-        if (resultCode == Activity.RESULT_OK) {
-            pendingResult.success(true);
-        } else if (resultCode == Activity.RESULT_CANCELED) {
-            pendingResult.success(Utils.isAccessibilitySettingsOn(context));
-        } else {
-            pendingResult.success(false);
+    @Override
+    public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_FOR_ACCESSIBILITY) {
+            if (resultCode == Activity.RESULT_OK) {
+                pendingResult.success(true);
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                pendingResult.success(Utils.isAccessibilitySettingsOn(context));
+            } else {
+                pendingResult.success(false);
+            }
+            return true;
         }
-        return true;
+        return false;
     }
-    return false;
-}
 
-@Override
-public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
-    this.mActivity = binding.getActivity();
-    binding.addActivityResultListener(this);
-    try {
-        FlutterEngineGroup enn = new FlutterEngineGroup(context);
-        DartExecutor.DartEntrypoint dEntry = new DartExecutor.DartEntrypoint(
-                FlutterInjector.instance().flutterLoader().findAppBundlePath(),
-                "accessibilityOverlay");
-        FlutterEngine engine = enn.createAndRunEngine(context, dEntry);
-        FlutterEngineCache.getInstance().put(CACHED_TAG, engine);
-        supportOverlay = true;
-    } catch (Exception exception) {
-        supportOverlay = false;
-        Log.e("ENGINE-ERROR", "onAttachedToActivity: " + exception.getMessage());
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        this.mActivity = binding.getActivity();
+        binding.addActivityResultListener(this);
+        try {
+            FlutterEngineGroup enn = new FlutterEngineGroup(context);
+            DartExecutor.DartEntrypoint dEntry = new DartExecutor.DartEntrypoint(
+                    FlutterInjector.instance().flutterLoader().findAppBundlePath(),
+                    "accessibilityOverlay");
+            FlutterEngine engine = enn.createAndRunEngine(context, dEntry);
+            FlutterEngineCache.getInstance().put(CACHED_TAG, engine);
+            supportOverlay = true;
+        } catch (Exception exception) {
+            supportOverlay = false;
+            Log.e("ENGINE-ERROR", "onAttachedToActivity: " + exception.getMessage());
+        }
     }
-}
 
-@Override
-public void onDetachedFromActivityForConfigChanges() {
-    this.mActivity = null;
-}
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        this.mActivity = null;
+    }
 
-@Override
-public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
-    onAttachedToActivity(binding);
-}
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        onAttachedToActivity(binding);
+    }
 
-@Override
-public void onDetachedFromActivity() {
-    this.mActivity = null;
-}
+    @Override
+    public void onDetachedFromActivity() {
+        this.mActivity = null;
+    }
 }
