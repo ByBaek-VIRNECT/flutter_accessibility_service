2fe76e1cba4a420cd7818212fe1deed00a326324
Beomyeol-VIRNECT <by100@virnect.com>
2025-04-07 17:49:54 +0900

    - code error 추가

--- android/src/main/java/slayer/accessibility/service/flutter_accessibility_service/AccessibilityListener.java
+++ android/src/main/java/slayer/accessibility/service/flutter_accessibility_service/AccessibilityListener.java
@@ -173,7 +173,7 @@ public class AccessibilityListener extends AccessibilityService {
                 broadcastIntent.putExtra(INTENT_GESTURE_RESULT, false);
                 broadcastIntent.putExtra(INTENT_GESTURE_ID, gestureId);
                 sendBroadcast(broadcastIntent);
-                return;
+                return START_STICKY;
             }
 
             GestureDescription.Builder gestureBuilder = new GestureDescription.Builder();
